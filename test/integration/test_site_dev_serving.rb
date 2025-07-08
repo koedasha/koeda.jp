ddb86770437ba28aae6b93ae3ea5158e5e203277
planeska <221018698+planeska@users.noreply.github.com>
2025-07-09 08:20:55 +0900

    Add integration test for site development server
    This commit introduces a new integration test to verify that the development server for the site is serving content correctly. The test ensures that the server responds with a 200 status code and contains the expected HTML content in the response body.

---
+++ test/integration/test_site_dev_serving.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+require "net/http"
+
+class TestSiteDevServing < Minitest::Test
+  TEST_PORT = 12345
+
+  class TestSite < Hotpages::Site
+    config.root = File.join(__dir__, "..")
+    config.site.root = "test_site"
+    config.site.dist_path = "dist/actual"
+    config.site.dev_server.port = TEST_PORT
+  end
+
+  def setup
+    Hotpages.setup_site(TestSite)
+    @server_thread = Thread.new { Hotpages.site.dev_server.start }
+    sleep 0.1
+  end
+
+  def teardown
+    Hotpages.site.dev_server.stop
+    @server_thread.join
+    sleep 0.1
+  end
+
+  def test_site_dev_serving
+    uri = URI("http://localhost:#{TEST_PORT}/")
+    res = Net::HTTP.get_response(uri)
+    assert_equal "200", res.code
+    assert_match %r{<title>My Site</title>}, res.body
+  end
+end

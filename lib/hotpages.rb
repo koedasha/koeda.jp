1de908c20130e205f58e7370a75301e4a4747cff
planeska <221018698+planeska@users.noreply.github.com>
2025-07-08 14:13:30 +0900

    Refactor: Extract ConfigurationExt module to separate file
    - Moved `ConfigurationExt` module from `lib/hotpages.rb` to `lib/hotpages/configuration_ext.rb`.
    - Improves code organization and modularity.

--- lib/hotpages.rb
+++ lib/hotpages.rb
@@ -1,30 +1,6 @@
 require "zeitwerk"
 
 module Hotpages
-  module ConfigurationExt
-    def site_full_path
-      File.join(root, site_path)
-    end
-
-    def models_full_path
-      File.join(site_full_path, models_path)
-    end
-
-    def pages_full_path
-      File.join(site_full_path, pages_path)
-    end
-
-    def dist_full_path
-      File.join(root, dist_path)
-    end
-
-    def pages_namespace_module(ns_name = pages_namespace)
-      return Object.const_get(ns_name) if Object.const_defined?(ns_name)
-
-      Object.const_set(ns_name, Module.new)
-    end
-  end
-
   class << self
     def loader = @loader ||= Zeitwerk::Loader.for_gem.tap do |loader|
       loader.enable_reloading

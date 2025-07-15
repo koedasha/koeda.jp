1144d87eb2627418ebe2953afc469b4570cf0717
planeska <221018698+planeska@users.noreply.github.com>
2025-07-15 20:41:49 +0900

    🚿

--- lib/hotpages/page.rb
+++ lib/hotpages/page.rb
@@ -11,20 +11,12 @@ class Hotpages::Page
 
     def inherited(subclass)
       subclass.layout_path = self.layout_path.dup if self.layout_path
-      subclass.helpers = self.helpers.dup if self.helpers
     end
 
     def layout(layout_path)
       @layout_path = layout_path
     end
     attr_accessor :layout_path
-
-    # Helper methods will be merged into the rendering context
-    def helper(*helper_modules)
-      @helpers ||= []
-      @helpers.concat(helper_modules)
-    end
-    attr_accessor :helpers
   end
 
   layout :site # Default layout path, can be overridden by individual pages

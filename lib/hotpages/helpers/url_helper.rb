725b491ad983703c5e6c2c72498beeede72b3947
planeska <221018698+planeska@users.noreply.github.com>
2025-07-15 21:36:09 +0900

    Refactor `link_to` method to support both text and URL arguments

--- lib/hotpages/helpers/url_helper.rb
+++ lib/hotpages/helpers/url_helper.rb
@@ -1,17 +1,21 @@
 module Hotpages::Helpers::UrlHelper
-  def link_to(url, **options, &block)
-    options[:href] = url
+  def link_to(text_or_url, url_or_nil = nil, **options, &block)
+    text, url = if url_or_nil
+                  [text_or_url, url_or_nil]
+                else
+                  [nil, text_or_url]
+                end
 
     if block_given?
-      tag.a options, &block
+      tag.a options.merge(href: url), &block
     else
-      tag.a options { url }
+      tag.a options { text || url }
     end
   end
 
   def link_to_page(page_path, **options, &block)
     unless config.page_base_class.exists?(page_path)
-      raise ArgumentError, "Page not found while generating link with 'link_to_page': #{page_path}"
+      raise "Page not found while generating link with 'link_to_page': #{page_path}"
     end
 
     link_to(page_path, **options, &block)

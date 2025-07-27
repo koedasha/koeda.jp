44feff1bdc0f9a5630a0b2d7cd371ec7c4728e5f
planeska <221018698+planeska@users.noreply.github.com>
2025-07-27 20:03:52 +0900

    refactor: remove custom inflector from site loader
    A custom inflector was required for file names like [slug], but it’s no longer necessary since we’ve changed it to __slug__.

--- lib/hotpages/site/loader.rb
+++ lib/hotpages/site/loader.rb
@@ -4,19 +4,8 @@ require "forwardable"
 class Hotpages::Site::Loader
   extend Forwardable
 
-  class Inflector < Zeitwerk::Inflector
-    def camelize(basename, abspath)
-      if basename =~ Hotpages::Page::Expandable::EXPANDABLE_BASENAME_REGEXP
-        super($1, abspath)
-      else
-        super
-      end
-    end
-  end
-
   def initialize(config:)
     @loader = Zeitwerk::Loader.new.tap do |loader|
-      loader.inflector = Inflector.new
       loader.push_dir(config.site.config_full_path)
       loader.push_dir(config.site.pages_full_path, namespace: config.site.pages_namespace_module)
       loader.push_dir(config.site.models_full_path)

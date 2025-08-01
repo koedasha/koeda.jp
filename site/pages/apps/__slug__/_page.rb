7a68fa80dd1de715a47b768deef62f3f5c7a5cb9
planeska <221018698+planeska@users.noreply.github.com>
2025-08-01 17:50:56 +0900

    Refactor: Move content_for calls to before_render in Page class to remove duplicates
    - Moved `content_for :app_name` and `content_for :app_image` from views to the `before_render` method in `Pages::Apps::Slug::Page` class.
    - Simplified `index.html.erb` and `privacy.html.erb` by removing redundant `content_for` calls.

--- site/pages/apps/__slug__/_page.rb
+++ site/pages/apps/__slug__/_page.rb
@@ -1,5 +1,10 @@
 class Pages::Apps::Slug::Page < Page
   layout :apps
 
+  def before_render
+    content_for :app_name, app.name_ja
+    content_for :app_image, app.image
+  end
+
   def app = Product.find_by_slug(segments[:slug])
 end

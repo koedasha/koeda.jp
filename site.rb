144d69f1b5bd7bf67d3baeef2fbe9bc89fe167c9
planeska <221018698+planeska@users.noreply.github.com>
2025-08-07 13:33:40 +0900

    feat: add English locale and i18n support for devs page
    - Added `site/locales/devs.en.yml` for English translations
    - Updated `site.rb` to configure i18n locales and default locale
    - Refactored `devs.html.erb` to use translation keys

--- site.rb
+++ site.rb
@@ -2,6 +2,8 @@ require "#{__dir__}/lib/hotpages"
 
 class Site < Hotpages::Site
   config.site.root = File.join(__dir__, "site")
+  config.site.i18n.locales = %w[ ja en ]
+  config.site.i18n.default_locale = "ja"
 end
 
 Hotpages.setup_site(Site)

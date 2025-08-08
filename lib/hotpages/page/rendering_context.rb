9edf32f9cf0f9a8c07fad2b4185459273921bbcd
planeska <221018698+planeska@users.noreply.github.com>
2025-08-08 19:55:28 +0900

    refactor: extract RenderingContext class to separate file

---
+++ lib/hotpages/page/rendering_context.rb
@@ -0,0 +1,43 @@
+class Hotpages::Page::RenderingContext
+  attr_reader :captured_contents
+  attr_accessor :cached_page_content
+
+  def initialize(page)
+    @page = page
+    @template_finder = Hotpages::Page::Template::Finder.new(page.base_path, page.site)
+    @cached_page_content = nil
+    @captured_contents = {}
+  end
+
+  def buffer = @buf
+
+  # TODO: support ruby objects responds to `render_in`
+  def render(template_path, **locals, &block)
+    template = template_finder.find!(template_path)
+
+    # TODO: block ignored warnings
+    if block_given?
+      template.render_in(self, locals, &block)
+    else
+      template.render_in(self, locals) do |content_name = nil|
+        if content_name
+          captured_contents[content_name.to_sym]
+        else
+          cached_page_content
+        end
+      end
+    end
+  end
+
+  private
+
+  attr_reader :page, :template_finder
+
+  def respond_to_missing?(name, include_private = false)
+    page.respond_to?(name, include_private)
+  end
+
+  def method_missing(method, *args, **kwargs, &block)
+    page.send(method, *args, **kwargs, &block)
+  end
+end

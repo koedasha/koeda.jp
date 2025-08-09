31cc2ed050de24f6bda346378e3079b61275b7cf
planeska <221018698+planeska@users.noreply.github.com>
2025-08-09 21:04:38 +0900

    docs: clarify error page instructions and editor link message
    Improved error page guidance for expanded pages and clarified editor link customization message.

--- lib/hotpages/dev_server.rb
+++ lib/hotpages/dev_server.rb
@@ -106,7 +106,7 @@ class Hotpages::DevServer
         <p><strong>Backtrace:</strong><br/>
           #{e.backtrace.map(&method(:render_backtrace_line)).join("\n")}
         </p>
-        <small><i>Editor link is customizable by <code>dev_server.backtrace_link_format</code> config</i></small>
+        <small><i>Customize editor links by <code>dev_server.backtrace_link_format</code> config</i></small>
       </body>
     HTML
   end
@@ -134,11 +134,11 @@ class Hotpages::DevServer
         <p><strong>Path:</strong> <code>#{req.path}</code></p>
         <p><strong>Unexpected result?</strong><br/>
           <ol style="">
-            <li>Make sure the path is correct.</li>
-            <li>Ensure the page class or template file exists under the `pages` directory structure.</li>
-            <li>For expanded pages, ensure module/class with `segment_names` class/module method exists for each expanded segment.</li>
-            <li>Ensure `segment_names` class/module method returns an array that includes the requested page/directory name.</li>
-            <li>Ensure `segments` key names are not duplicated within nested directory hierarchies. e.g. This is invalid structure: `users/__id__/posts/__id__`</li>
+            <li>Ensure the path is correct. Files whose names start with `_` are ignored.</li>
+            <li>Ensure the page class or template file exists within the `pages` directory structure.</li>
+            <li>For expanded pages, ensure a module or class with the `segment_names` class/module method exists for each expanded segment.</li>
+            <li>Ensure the `segment_names` class/module method returns an array that includes the requested page or directory name.</li>
+            <li>Ensure `segments` key names are not duplicated within nested directory hierarchies. For example, this structure is invalid: `users/__id__/posts/__id__`.</li>
           </ol>
         </p>
       </body>

b7597fa19d8f08f07e6fd6f989d76d02a20f0914
planeska <221018698+planeska@users.noreply.github.com>
2025-08-12 17:57:34 +0900

    testing: use eagerLoadControllersFrom for Stimulus controller loading
    Replaces manual registration of RemoteModalController with eager loading of all controllers from the "controllers" directory using stimulus-loading. Updates references in test and expected files to reflect new asset hash.

--- test/test_site/assets/site.js
+++ test/test_site/assets/site.js
@@ -1,13 +1,12 @@
 import "@hotwired/turbo"
 import { Application } from "@hotwired/stimulus"
-import RemoteModalController from "controllers/remote_modal_controller.js"
+import { eagerLoadControllersFrom } from "stimulus-loading"
 
 const application = Application.start()
+eagerLoadControllersFrom("controllers", application)
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-application.register("remote-modal", RemoteModalController)
 
 export { application }

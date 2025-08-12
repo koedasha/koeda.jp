bb34b16ca30ca20a87dd7297214a4cf528f4008c
planeska <planeska@users.noreply.github.com>
2025-08-12 07:01:44 +0000

    deploy: bafd0a711d4c48f6729d970a6db22f5d6a2a0182

--- assets/site.js
+++ assets/site.js
@@ -1,13 +1,13 @@
 import "@hotwired/turbo"
 import { Application } from "@hotwired/stimulus"
-import RemoteModalController from "controllers/remote_modal_controller.js"
+// import RemoteModalController from "controllers/remote_modal_controller.js"
+import { eagerLoadControllersFrom } from "stimulus-loading"
 
 const application = Application.start()
+eagerLoadControllersFrom("controllers", application)
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-application.register("remote-modal", RemoteModalController)
 
 export { application }

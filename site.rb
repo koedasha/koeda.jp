365b168fc6acbfac0891d1ccbfbef74f77e9f7ac
planeska <221018698+planeska@users.noreply.github.com>
2025-08-20 11:12:18 +0900

    feat: add TemplatePathAnnotation extension and enable gem development mode
    - Added site/extensions/template_path_annotation.rb as a sample site-specific extension that annotates rendered templates with their absolute path.
    - Updated site.rb to load and register the new extension.
    - Modified bin/dev to start the dev server in gem development mode by default.

--- site.rb
+++ site.rb
@@ -3,6 +3,11 @@ require "tilt"
 
 Tilt.register(Tilt::KramdownTemplate, "md")
 
+# TODO: loader setup for site specific extensions
+Hotpages.loader.push_dir("#{__dir__}/site/extensions")
+Hotpages.reload
+Hotpages.extensions << TemplatePathAnnotation
+
 class Site < Hotpages::Site
   config.site.i18n.locales = %w[ ja en ]
   config.site.i18n.default_locale = "ja"

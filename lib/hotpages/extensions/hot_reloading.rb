cd2395cfe39926154bb40e1067c44b50dafb8fe1
planeska <221018698+planeska@users.noreply.github.com>
2025-08-26 00:24:44 +0900

    WIP: feat: replace WebSocket hot reloading with SSE
    - Remove custom WebSocket implementation for hot reloading
    - Use Server-Sent Events (SSE) via EventSource for file change notifications
    - Update Ruby backend to stream file changes to connected clients
    - Update frontend JS to listen for SSE and reload assets accordingly

--- lib/hotpages/extensions/hot_reloading.rb
+++ lib/hotpages/extensions/hot_reloading.rb
@@ -1,4 +1,5 @@
 require "listen"
+require "json"
 
 # TODO: E2E Testing
 module Hotpages::Extensions::HotReloading
@@ -6,13 +7,13 @@ module Hotpages::Extensions::HotReloading
 
   prepending to: "Hotpages::DevServer"
 
-  HOT_RELOADING_JS = "_hot_reloading.js"
+  FILE_CHANGES_PATH = "/_file_changes"
+  HOT_RELOADING_JS_PATH = "/_hot_reloading.js"
 
   def start(gem_development: false)
     logger.info "Hot reloading enabled"
 
-    @web_socket = WebSocket.new
-    @web_socket_url = "ws://#{host}:#{port}"
+    @file_changes_streams = []
     # Set wait_for_delay to 0.2 seconds for more stable hot reloading
     @file_listener = Listen.to(site.root, wait_for_delay: 0.2) do |modified, added, removed|
       (modified + added + removed).each do |changed_file|
@@ -24,33 +25,55 @@ module Hotpages::Extensions::HotReloading
     super
   end
 
-  def stop
-    web_socket.close
-    super
-  end
-
   private
 
-  attr_reader :web_socket, :web_socket_url, :file_listener
+  attr_reader :file_changes_streams, :file_listener
+
+  class FileChangesStream < StringIO
+    def readpartial(len, buf = "")
+      partial = nil
+
+      # TODO: Cannot stop serving with INT
+      while !partial
+        rewind
+        partial = read_nonblock(len, buf, exception: false)
+        sleep 0.1
+      end
+
+      self.string.clear
+
+      partial
+    end
+  end
 
   def page_content(page)
     content = super
 
     hot_reload_scripts = <<~HTML
-      <script>window._HOTPAGES_HOT_RELOADING_WS_URL="#{web_socket_url}"</script>
-      <script src=\"/#{HOT_RELOADING_JS}\" type=\"module\"></script>
+      <script>window._HOTPAGES_FILE_CHANGES_PATH="#{FILE_CHANGES_PATH}"</script>
+      <script src=\"#{HOT_RELOADING_JS_PATH}\" type=\"module\"></script>
     HTML
     content.sub(/<\/head>/, "#{hot_reload_scripts}\n</head>")
   end
 
-  def handle_request(req, res)
-    if req.path == "/#{HOT_RELOADING_JS}"
+  def setup_routes
+    super
+
+    server.mount_proc HOT_RELOADING_JS_PATH do |req, res|
+      res["Cache-Control"] = "no-store"
       res["Content-Type"] = "application/javascript"
-      res.body = File.read(File.join(__dir__, "hot_reloading", HOT_RELOADING_JS))
-    elsif web_socket.handshake_request?(req)
-      web_socket.handshake(req, res)
-    else
-      super(req, res)
+      res.body = File.read(File.join(__dir__, "hot_reloading", HOT_RELOADING_JS_PATH))
+    end
+
+    server.mount_proc FILE_CHANGES_PATH do |req, res|
+      res["Content-Type"] = "text/event-stream"
+      res.chunked = true
+      res.keep_alive = true
+
+      FileChangesStream.new.tap do |stream|
+        file_changes_streams << stream
+        res.body = stream
+      end
     end
   end
 
@@ -60,12 +83,20 @@ module Hotpages::Extensions::HotReloading
     if file_path.start_with?(site.assets_path.to_s)
       case file_path
       when /\.css$/
-        web_socket.broadcast(action: "reload:css", path: file_path_to_notify)
+        broadcast_file_change(action: "reload:css", path: file_path_to_notify)
       when /\.js$/
-        web_socket.broadcast(action: "reload:js", path: file_path_to_notify)
+        broadcast_file_change(action: "reload:js", path: file_path_to_notify)
       end
     else
-      web_socket.broadcast(action: "reload:html")
+      broadcast_file_change(action: "reload:html")
+    end
+  end
+
+  def broadcast_file_change(payload)
+    file_changes_streams.each do
+      it.write("data: #{JSON.generate(payload)}\n\n")
+    rescue IOError
+      file_changes_streams.delete(it)
     end
   end
 end

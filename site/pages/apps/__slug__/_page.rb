eacacb8ba5e087a415401a4d1620b0e3afc3c82d
planeska <221018698+planeska@users.noreply.github.com>
2025-08-28 21:07:56 +0900

    fix: localize app title in set_contents method

--- site/pages/apps/__slug__/_page.rb
+++ site/pages/apps/__slug__/_page.rb
@@ -8,6 +8,8 @@ class Pages::Apps::Slug::Page < Page
   private
 
   def set_contents
+    # TDOO: Localize
+    content_for :title, app.name_ja
     content_for :app_name, app.name_ja
     content_for :app_image, app.image
   end

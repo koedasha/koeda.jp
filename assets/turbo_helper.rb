0263f0fa57d3967a7390de368baebb3c00d40e23
planeska <planeska@users.noreply.github.com>
2025-09-02 02:21:02 +0000

    deploy: ad1845f0e48b70a5e426fbcd3311aff03cc2b248

--- assets/turbo_helper.rb
+++
@@ -1,6 +0,0 @@
-module Hotpages::Extensions::Hotwire::TurboHelper
-  def turbo_frame_tag(id, **options, &block)
-    attributes = { id: id.to_s.tr("_", "-"), **options }
-    tag.turbo_frame id:, **attributes, &block
-  end
-end

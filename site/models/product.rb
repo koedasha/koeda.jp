522353a2e867d263337da0bf55f14a80a507416e
planeska <221018698+planeska@users.noreply.github.com>
2025-09-25 21:43:53 +0900

    Refactor file paths for terms and privacy methods
    - Updated the file paths in the `terms` and `privacy` methods to reflect the new location of the markdown files in the `data/apps` directory.
    - Removed references to the old `shared/apps` directory.

--- site/models/product.rb
+++ site/models/product.rb
@@ -36,13 +36,13 @@ Product = Data.define(
 
   def terms(locale:)
     # TODO: localize
-    # "shared/apps/#{slug.gsub("-", "_")}/terms_#{locale}.md"
-    "shared/apps/#{slug.gsub("-", "_")}/terms_ja.md"
+    # "data/apps/#{slug.gsub("-", "_")}/terms_#{locale}.md"
+    "data/apps/#{slug.gsub("-", "_")}/terms_ja.md"
   end
 
   def privacy(locale:)
     # TODO: localize
-    # "shared/apps/#{slug.gsub("-", "_")}/privacy_#{locale}.md"
-    "shared/apps/#{slug.gsub("-", "_")}/privacy_ja.md"
+    # "data/apps/#{slug.gsub("-", "_")}/privacy_#{locale}.md"
+    "data/apps/#{slug.gsub("-", "_")}/privacy_ja.md"
   end
 end

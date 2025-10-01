221b5f1bb901fc281bb187b2c9b86bb114b9f6f4
planeska <221018698+planeska@users.noreply.github.com>
2025-10-01 15:12:16 +0900

    Refactor and update app assets and product model
    - Refactor CSS classes for app description and links.
    - Replace `elapsed-times-screenshot-ja.png` with `promo-1.ja.webp`.
    - Rename `elapsed-times.png` to `app-icon.png`.
    - Add `ios_url` attribute to `Product` model.
    - Update `Product` data with new image paths and description.
    - Modify app page to include App Store link or placeholder text.

--- site/models/product.rb
+++ site/models/product.rb
@@ -1,6 +1,6 @@
 # App
 Product = Data.define(
-  :slug, :image,
+  :slug, :image, :ios_url,
   :name_ja, :copy_ja, :desc_ja, :store_url_jp, :screenshot_ja
 ) do
   class << self
@@ -8,21 +8,21 @@ Product = Data.define(
       @products ||= [
         new(
           slug: "elapsed-times",
-          image: "images/elapsed-times.png",
+          image: "images/elapsed-times/app-icon.png",
+          ios_url: nil,
           name_ja: "時間計測タイマー",
-          copy_ja: "「あれからどれくらいたったかな？」日常生活の中で時間を計測したいときに役立つタイマーアプリ。",
+          copy_ja: "ワンタップで計測開始。日常生活の中で時間を計測したいときに役立つタイマーアプリ。",
           desc_ja: <<~MD,
           「あれからどれくらいたったかな？」
 
           日常生活の中でそんなふうに思う瞬間はないでしょうか。
 
-        時間計測タイマーは、時間を測るという目的に特化したアプリです。
+          時間計測タイマーは、時間を測るという目的に特化したアプリです。
 
           グループにまとめた複数のタイマーでの同時計測やメモ機能、充実した通知機能により、さまざまな時間を測りたいシーンでお使いいただけます。
           MD
           store_url_jp: "https://apps.apple.com/jp/app/twitter/id333903271",
-          # screenshot_ja: "elapsed-times-screenshot-ja.png"
-          screenshot_ja: "images/ph.svg"
+          screenshot_ja: "images/elapsed-times/promo-1.ja.webp"
         )
       ]
     end
